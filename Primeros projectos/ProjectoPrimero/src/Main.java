b55962ee260c43a624b0dad400d2a8654b2eee8b
carlosalvmaz <carlos.alvmaz@gmail.com>
2025-11-06 12:14:39 +0100

    Update Main.java

--- Primeros projectos/ProjectoPrimero/src/Main.java
+++ Primeros projectos/ProjectoPrimero/src/Main.java
@@ -1,10 +1,39 @@
 
+//ESto es un comentario de una sola linea
+//SHift alt y flecha para bajar una linea
+/*
+Comentario mas largo
+ */
+
+/**
+ *
+ */
 
 public class Main {
 
     public static void  main(String[] args) {
- System.out.println("Hola, este es mi primer programa en Java");
+
+        String nombreCompleto = "Borja"; /* Los string son palabras y
+         van entrecomillaods dobles*/
+        int edad = 19; //numero entero
+        int edadmental = edad+1;
+         float altura = 1.71f; //numeros con decimales
+        double peso = 1.7111123;
+
  System.out.println("Esta es mi segunda linea");
+ System.out.println("mi edad mental son "+edadmental);
+System.out.println("mi peso es  "+peso);
+ System.out.println("Mi edad son "+edad);
+
+ System.out.println("Hola, este es mi primer programa en Java");
+
+ System.out.print("Mi nombre es "+nombreCompleto);
 
 }
+
+public void metodoCarton()
+
+    {
+        //TODO pendiente crear el carton con numeros aleatorios sin repetir
+    }
 }
