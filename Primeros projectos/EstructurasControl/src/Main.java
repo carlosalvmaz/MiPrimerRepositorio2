920ffc0c19852bcfc4c4bcb61464e088f9305bd1
carlosalvmaz <carlos.alvmaz@gmail.com>
2026-01-27 12:49:16 +0100

    a

--- Primeros projectos/EstructurasControl/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[]args){
-        SentenciaIF sentenciaIF = new SentenciaIF();
-        //sentenciaIF.examenAprobado(9);
-        sentenciaIF.notaExamne(100);
-        SentenciaSwitch sentenciaSwitch = new SentenciaSwitch();
-        sentenciaSwitch.menuOpciones();
-    }
-}
