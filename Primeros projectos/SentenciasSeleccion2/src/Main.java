97fb4a1bc231d9cbe8491490267f931911a862b8
carlosalvmaz <carlos.alvmaz@gmail.com>
2026-01-28 00:00:21 +0100

    a

--- Primeros projectos/SentenciasSeleccion2/src/Main.java
+++ Primeros projectos/SentenciasSeleccion2/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-public  static void main(String[] args) {
-    SentenciaIF sentenciaIF = new SentenciaIF();
-}
+    public static void main(String[] args) {
+SentenciaIF sentenciaIF = new SentenciaIF();
+sentenciaIF.examenAprobado(7);
+    }
 }
--- Primeros projectos/SentenciasSeleccion2/src/SentenciaIF.java
+++ Primeros projectos/SentenciasSeleccion2/src/SentenciaIF.java
@@ -1,5 +1,10 @@
 public class SentenciaIF {
     public void examenAprobado(int nota){
-
+        System.out.println("PRocedemos a evaluar si un examen esta OK");
+        System.out.println("La nota que vamos a evaluar es:"+nota );
+        if(nota>=5){
+            System.out.println("El examen esta aprobado");
+        }
+        System.out.println("Programa finalizado");
     }
 }
