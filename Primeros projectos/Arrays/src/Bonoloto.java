1dd278985eb5a3c4575b0899e9e60cedb5278db6
carlosalvmaz <carlos.alvmaz@gmail.com>
2026-03-26 10:17:40 +0100

    a

--- Primeros projectos/Arrays/src/Bonoloto.java
+++ Primeros projectos/Arrays/src/Bonoloto.java
@@ -3,34 +3,41 @@ import java.util.Scanner;
 public class Bonoloto {
     private int[]numerosSistema = new int[5];// [0.0.0.0.0]
    private int[]numerosUsuario = new int[5];// [0.0.0.0.0]
-
+int aciertos = 0;
     private Scanner scanner = new Scanner(System.in);
-    public void buscarNumero(int numero){
-         //buscar un numero deltro dela rray numeros sistema
+    public void iniciarJuego(){
+        //al iniciar el juegvpo pasan dos cosas
+        //1-saler de la maquina 5 numeros que seran los numeros del sistema del 0 al 20
+        for(int i=0;i<5;i++){
+            numerosSistema[i]=(int)(Math.random()*21);//generamos numeros aleatorios del 1 al 20
+        }
+        //2.- Se le piden 5 numeros al usuario
+        System.out.println("Procedemos a sellar tu boleto");
+        for(int i=0;i<5;i++){
+            System.out.println("introduce un numero: ");
+            numerosUsuario[i]=scanner.nextInt();
+        }
+    System.out.println("Procedemos a ver las condiciones del sistema ");
+        for (int item: numerosUsuario){
+            buscarNumero(item);
+        }
+    }
+    public void buscarNumero(int numero) {
+         //buscar un numero deltro del array numeros sistema
         //numerosSistema -> variable clase
         //numero-> parametro
-        int aciertos=0;
+        //3
+        //4,5,12,20,3
+
         for(int item:numerosSistema){
             if(item==numero){
                 aciertos++;
+                break;
             }
         }
 
 
     }
-public void iniciarJuego(){
-    //al iniciar el juegvpo pasan dos cosas
-    //1-saler de la maquina 5 numeros que seran los numeros del sistema del 0 al 20
-    for(int i=0;i<5;i++){
-        numerosSistema[i]=(int)(Math.random()*21);//generamos numeros aleatorios del 1 al 20
-    }
-    //2.- Se le piden 5 numeros al usuario
-    System.out.println("Procedemos a sellar tu boleto");
-    for(int i=0;i<5;i++){
-        numerosUsuario[i]=scanner.nextInt();
-    }
-
-}
 public void listarNumerosSistema(){
     for(int item:numerosSistema){
         System.out.println(item);
@@ -38,6 +45,10 @@ public void listarNumerosSistema(){
 
 }
     public void listarNumerosUsuario(){
+for(int item:numerosUsuario){
+    System.out.println(item);
 
+}
+        System.out.println("Has acertado: "+aciertos+"numeros");
     }
 }
--- Primeros projectos/Arrays/src/MainBonoloto.java
+++ Primeros projectos/Arrays/src/MainBonoloto.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class MainBonoloto {
     public static void main(String[] args) {
         Bonoloto bonoloto = new Bonoloto();
-                bonoloto.iniciarJuego();
-                bonoloto.listarNumerosSistema();
+        Scanner scanner =new Scanner(System.in);
+        boolean jugar = false;
+        do {
+            bonoloto.iniciarJuego();
+            System.out.println("Los numeros del sistema son: ");
+            bonoloto.listarNumerosSistema();
+            System.out.println("Los numeros del usuario son: ");
+            bonoloto.listarNumerosUsuario();
+            System.out.println();
+
+            System.out.println("Quieres jnugar otra vez");
+            jugar = scanner.nextBoolean();
+        }while(jugar);
     }
 
 }
